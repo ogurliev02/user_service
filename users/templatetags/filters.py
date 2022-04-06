1ed7dfada1ccb32894ff56e2f02478de814f03be
Gamid Ogurliev <gogurliev@rbc.ru>
2022-04-06 22:11:38 +0300

    Допиливание функционала

--- users/templatetags/filters.py
+++ users/templatetags/filters.py
@@ -3,10 +3,11 @@ from django import template
 register = template.Library()
 
 
-def perfect_view(value):
-    result = value.__dict__
+def user_to_json(user, add_fields=None):
+    result = user.__dict__
     del result['_state']
+    result.update(add_fields)
     return result
 
 
-register.filter('perfect_view', perfect_view)
+register.filter('user_to_json', user_to_json)
--- users/views.py
+++ users/views.py
@@ -7,12 +7,14 @@ from django.views import View
 
 class UserView(View):
     def get(self, request, *args, **kwargs):
-        page_number = request.GET.get('page')
+        page_number = int(request.GET.get('page'))
         info_per_page = int(request.GET.get('info_per_page') or 25)
-        if not page_number or not info_per_page:
-            return HttpResponseNotFound('<h1>Введите, пожалуйста, аттрибуты page_number и/или info_per_page</h1>')
-        users = User.objects.all().select_related('profile').defer('password')
+        if not page_number:
+            return HttpResponseNotFound('<h1>Введите, пожалуйста, аттрибут page</h1>')
+        users = User.objects.all().only('email')
         paginator = Paginator(users, info_per_page)
+        if page_number > paginator.num_pages:
+            return HttpResponseNotFound('<h1>У нас нет столько страниц!</h1>')
         page_obj = paginator.get_page(page_number)
         return render(request, 'list.html', {'page_obj': page_obj})
 
@@ -20,8 +22,9 @@ class UserView(View):
 class EmailView(View):
     def get(self, request, *args, **kwargs):
         email = request.GET.get('email') or 'пусто!'
-        user = User.objects.filter(email=email).first()
+        user = User.objects.filter(email=email).select_related('profile').first()
+        add_fields = {'colors': user.profile.favorite_colors, 'sex': user.profile.sex}
         if not user:
             return HttpResponseNotFound(f'<h1>По вашему email - {email} ничего не найдено'
                                         f', введите пожалуйста корректный email</h1>')
-        return render(request, 'info.html', {'user': user})
+        return render(request, 'info.html', {'user': user, 'add_fields': add_fields})
